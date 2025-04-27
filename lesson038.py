0b86b46589eb637e8eeb1bc5f1a59a580b1f2e4f
Mark Peters <mapeters101@earthlink.net>
2025-04-26 21:13:24 -0400

    Problem statement for lesson038.

---
+++ lesson038.py
@@ -0,0 +1,7 @@
+"""
+The aim is to design an algorithm that can return the maximum item of a stack in O(1) running time complexity. We can use O(N) extra memory!
+
+Hint: we can use another stack to track the max item!
+
+Good luck!
+"""
